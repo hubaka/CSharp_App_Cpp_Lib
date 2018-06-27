68b246673597952f206376309cb015d72ffd6e63
Anand Kathiresan <aillanan@yahoo.com>
2018-06-27 17:14:17 +0530

    initial_commit

---
+++ application/appmain/source/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("English");
+            comboBox1.Items.Add("Spanish");
+            comboBox1.Items.Add("French");
+            comboBox1.SelectedIndex = 0;
+    }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "English")
+            {
+                ChangeLanguage("en");
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Spanish")
+            {
+                ChangeLanguage("es-ES");
+            }
+            else
+            {
+                ChangeLanguage("fr-FR");
+            }
+        }
+
+        private void ChangeLanguage(string lang)
+        {
+            foreach (Control c in this.Controls)
+            {
+                ComponentResourceManager resources = new ComponentResourceManager(typeof(Form1));
+                resources.ApplyResources(c, c.Name, new CultureInfo(lang));
+            }
+        }
+
+    }
+}
